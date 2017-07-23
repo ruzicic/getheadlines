a9268073bfa04f914c80a667e0de068aca18b3d2
ruzicic <ruzicic@gmail.com>
2017-07-23 18:28:11 +0200

    exposed two routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const helmet = require('koa-helmet');
 
 const {logger, morgan} = require('./scripts/logger');
 const {initializeApp} = require('./scripts/main');
+
+const {validateFeedRoute} = require('./scripts/middlewares');
+const {ProvidersHandler, FeedsHandler} = require('./scripts/handlers');
 
 const port = 3028;
 const app = new Koa();
@@ -10,14 +14,19 @@ const apiRouter = new Router({
 	prefix: '/api'
 });
 
+app.use(helmet());
+app.use(morgan);
 app.use(apiRouter.routes());
 app.use(apiRouter.allowedMethods());
 
-// Logging middleware
-logger.debug(`Setting up Morgan logger`);
-app.use(morgan);
-
 initializeApp();
+
+// API Router
+apiRouter.get('/providers', ProvidersHandler.getAll);
+apiRouter.get('/feeds/:provider/:category', validateFeedRoute, FeedsHandler.get);
+
+// Everything else, not covered by apiRouter.routes()
+app.use(ctx => ctx.status = 401);
 
 app.listen(port, () => {
 	logger.info(`App running on port ${port}`);
