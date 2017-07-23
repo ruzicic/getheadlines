92ab3e16da6feaa42ea79bec503996cec1bb0538
ruzicic <ruzicic@gmail.com>
2017-07-23 18:28:45 +0200

    note to self.

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ const initializeApp = async () => {
 	const activeProviders = await getActiveProviders();
 	await createRoutes(activeProviders);
 	await scheduleJobs(activeProviders);
+
+	// TODO: Create Cron Job (once a day):
+	// Clear feed data older than 3 days
 };
 
 module.exports = {
