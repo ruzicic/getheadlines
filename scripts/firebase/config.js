80902949cb4fd65f26057736f6202e150a5ae6b9
ruzicic <ruzicic@gmail.com>
2017-08-09 23:03:29 +0200

    Wrap firebase init in try/catch

--- scripts/firebase/config.js
+++ scripts/firebase/config.js
@@ -1,30 +1,35 @@
-const fs = require('fs');
 const path = require('path');
 const firebase = require('firebase-admin');
-
-// Firebase Service Account cofiguration (download your own at Firebase Console)
-const firebaseConfig = path.join(__dirname, 'serviceAccountKey.json');
 
 // Firebase database name
 const databaseURL = 'https://getheadlines-16e96.firebaseio.com';
 
-// Will be assigned config file path, after validating it exists
+// Firebase Service Account cofiguration
+// Download your own at Firebase Console
+const serviceAccountPath = path.join(__dirname, 'serviceAccountKey.json');
 let serviceAccount;
 
-// Check if Firebase Service Account config exist
 try {
-	fs.statSync(firebaseConfig);
-	serviceAccount = require(firebaseConfig);
-} catch(err) {
+	serviceAccount = require(serviceAccountPath);
+} catch (err) {
 	console.error('Firebase serviceAccountKey.json NOT FOUND.');
 	console.error('Go to https://firebase.google.com/docs/admin/setup for more info');
-	console.error(err);	
+	console.error(err);
+	process.exit(1);
 }
 
-firebase.initializeApp({
-	credential: firebase.credential.cert(serviceAccount),
-	databaseURL: databaseURL
-});
+// Initialize Firebase App
+try {
+	firebase.initializeApp({
+		credential: firebase.credential.cert(serviceAccount),
+		databaseURL: databaseURL
+	});
+} catch (err) {
+	console.error('Could not initialize Firebase App.');
+	console.error('Go to https://firebase.google.com/docs/reference/admin/node/admin#.initializeApp for more info');
+	console.error(err);
+	process.exit(1);
+}
 
 // For DEBUG only
 // firebase.database.enableLogging(true);
