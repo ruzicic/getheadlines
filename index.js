480461d0fa4bad101bb0b0369d2d9ad0454ab92d
ruzicic <ruzicic@gmail.com>
2017-08-12 22:20:51 +0200

    add comments for API documentation

--- index.js
+++ index.js
@@ -22,8 +22,47 @@ const app = new Koa()
 
 initializeApp();
 
-// API Router
+/**
+ * @api {get} /providers Get All Providers
+ * @apiVersion 1.0.0
+ * @apiName Providers
+ * @apiGroup Providers
+ * 
+ * @apiDescription Get a list of all available feed providers
+ * 
+ * @apiExample Example usage:
+ * curl --header "Authorization: YOUR-API-KEY" https://getheadlines.io/api/blic/zabava
+ * 
+ * @apiHeader {String} Authorization Authorization token - Your personal API key
+ * 
+ * @apiError NoToken 403 Authorization header not found
+ * @apiError BadToken 403 Bad token provided
+ * 
+ * @apiSuccess {Object[]} providers List of all active providers
+ */
 apiRouter.get('/providers', validateToken, ProvidersHandler.getAll);
+
+/**
+ * @api {get} /feeds/:provider/:category Get feeds
+ * @apiVersion 1.0.0
+ * @apiName GetFeeds
+ * @apiGroup Feeds
+ * 
+ * @apiDescription Get all feeds for provider and its category
+ * 
+ * @apiExample Example usage:
+ * curl --header "Authorization: YOUR-API-KEY" https://getheadlines.io/api/blic/zabava
+ * 
+ * @apiHeader {String} Authorization Authorization token - Your personal API key
+ * 
+ * @apiError NoToken 403 Authorization header not found
+ * @apiError BadToken 403 Bad token provided
+ * 
+ * @apiSuccess {String} provider Provider name
+ * @apiSuccess {String} category Category of provider
+ * @apiSuccess {Object[]} query Query for which request is returned
+ * @apiSuccess {Object[]} data List of feeds
+ */
 apiRouter.get('/feeds/:provider/:category', validateToken, validateFeedRoute, FeedsHandler.get);
 
 // Everything else, not covered by API Router
