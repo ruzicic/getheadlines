b6ceb8e7c66d0d16f6b2817d5d731f2d2ea13b44
ruzicic <ruzicic@gmail.com>
2017-08-13 15:28:40 +0200

    updated folder structure

--- clientApp/src/assets/js/home.js
+++ clientApp/src/assets/js/home.js
@@ -1,31 +1,9 @@
-const firebase = require('firebase/app');
 const Slideout = require('slideout');
-require('firebase/auth');
-require('./style.scss');
-require.context('./assets/images/', false); // true for subfolders
 
-// console.log('App.js loaded');
+import FBApp from './app.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     document.body.removeAttribute('unresolved');
-
-    const config = {
-        projectId: "getheadlines-16e96",
-        apiKey: "AIzaSyAhMYzerYXgO4vzp5o7Tf-o19Fr1t34iT8",
-        authDomain: "getheadlines-16e96.firebaseapp.com",
-        databaseURL: "https://getheadlines-16e96.firebaseio.com"
-    };
-
-    firebase.initializeApp(config);
-
-    try {
-        let app = firebase.app();
-        let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-        // console.log(`Firebase initialized with modules: ${features.join(', ')}`);
-    } catch (err) {
-        console.error('Error loading the Firebase SDK, check the console.');
-        console.error(err);
-    }
 
     // Register Slideout.js - off canvas menu
     const mainElem = document.getElementById('main');
@@ -59,3 +37,5 @@ document.addEventListener('DOMContentLoaded', function () {
             mainElem.removeEventListener('click', close);
         });
 });
+
+console.log(FBApp);
