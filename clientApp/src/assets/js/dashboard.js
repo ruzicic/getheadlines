96233d6a107a89b65a174e66a0c23474173c52a4
ruzicic <ruzicic@gmail.com>
2017-08-13 20:20:19 +0200

    add avatar placeholder

--- clientApp/src/assets/js/dashboard.js
+++ clientApp/src/assets/js/dashboard.js
@@ -1,5 +1,8 @@
+import '../styles/dashboard.scss';
 import { firebase, FBApp } from './firebaseApp.js';
 const Clipboard = require('clipboard');
+
+const avatarPlaceholder = 'https://api.adorable.io/avatars/40/abott@adorable.png';
 
 const initAuth = () => {
     const userAvatar = document.getElementById('userAvatar');
@@ -15,25 +18,28 @@ const initAuth = () => {
             // Reveal content if user is signed in
             document.body.removeAttribute('unresolved');
 
-            // User is signed in.
             userName.textContent = user.displayName;
             userEmail.textContent = user.email;
             userEmailVerified.textContent = user.emailVerified;
             userID.textContent = user.uid;
 
+            // Check if user has image
             if (user.photoURL) {
                 userAvatar.src = user.photoURL;
+            } else {
+                userAvatar.src = avatarPlaceholder;
             }
 
+            // Attach ClipboardJS instance to "copy" api key button
             new Clipboard('#apiKeyCopy');
-
             signOutButton.addEventListener('click', () => signOut());
 
+            // Get and display user token
             user.getIdToken().then(accessToken => {
                 userTokenInput.value = accessToken;
             });
         } else {
-            // User is not signed in.
+            // User is not signed in
             window.location.href = '/auth.html';
         }
     }, error => {
