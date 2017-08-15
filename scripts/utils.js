017413ab4464f61f41b488d5532692b8c866568d
ruzicic <ruzicic@gmail.com>
2017-08-15 19:13:42 +0200

    added url validation, before rss-parser

--- scripts/utils.js
+++ scripts/utils.js
@@ -3,6 +3,11 @@ const parser = require('rss-parser');
 const {logger} = require('./logger');
 
 const fetchFeed = url => {
+	if (!url) {
+		logger.error(`URL not provided to rss-parser.`);
+		process.exit(1);
+	}
+
 	return new Promise((resolve, reject) => {
 		parser.parseURL(url, (err, parsed) => {
 			if (err) {
