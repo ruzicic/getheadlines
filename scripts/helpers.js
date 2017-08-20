1f6ebd82b09d241fb222e4eebd96876fc1b88ec8
ruzicic <ruzicic@gmail.com>
2017-08-20 22:50:05 +0200

    add sanitize-html

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,3 +1,5 @@
+const sanitizeHtml = require('sanitize-html');
+
 /**
  * @method addMinutesToDate
  * @param {Date} date - Date object. Default NOW
@@ -8,6 +10,30 @@ const addMinutesToDate = (date = new Date(), add = 60) => {
 	return new Date(date.getTime() + (add * 60000));
 };
 
+// Sanitize HTML
+const cleanHTML = data => {
+	if (!data) {
+		logger.error(`Data not provided to sanitize-html.`);
+		process.exit(1);
+	}
+
+	const config = {
+		allowedTags: ['h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'a', 'ul', 'ol',
+			'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br',
+			'pre'],
+		allowedAttributes: {
+			a: ['href', 'name', 'target'],
+			img: ['src', 'alt']
+		},
+		selfClosing: ['img', 'br', 'hr', 'area', 'base', 'basefont', 'input', 'link', 'meta'],
+		allowedSchemes: ['http', 'https', 'mailto'],
+		allowProtocolRelative: true
+	}
+
+	return sanitizeHtml(data, config);
+}
+
 module.exports = {
-	addMinutesToDate
+	addMinutesToDate,
+	cleanHTML
 };
