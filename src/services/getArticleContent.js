b0d11e83def8065bb4e8745398e075e085c6d54f
Mladen Ruzicic <ruzicic@gmail.com>
2018-04-01 18:56:05 +0200

    Add fetchFeed and getArticleContent services

---
+++ src/services/getArticleContent.js
@@ -0,0 +1,52 @@
+const read = require('node-readability');
+const { isValidUrl, cleanHTML } = require('../utils');
+const logger = require('../../lib/logger');
+
+/**
+ * Article Object
+ *
+ * @typedef {Object} Article Article Object, returned by node-readability
+ * @property {String} content The article content of the web page. False if failed!
+ * @property {String} title The article title of the web page.
+ * @property {String} textBody A string containing all the text found on the page
+ * @property {String} html The original html of the web page.
+ * @property {String} document
+ */
+
+/**
+ * Uses node-readability library to scrape web page
+ * https://github.com/luin/readability
+ *
+ * @method getArticle
+ * @param {String} url Valid URL
+ * @returns {Promise.<Article>} Promise that returns an Article Content if resolved
+ */
+const getArticle = url => new Promise((resolve) => {
+	if (!isValidUrl(url)) {
+		logger.error(`[getArticle] Invalid URL provided ${url}.`);
+		resolve();
+	}
+
+	read(url, (err, article) => {
+		if (err) {
+			logger.error(`[getArticle] Error getting article content for url: ${url}`, err);
+			resolve();
+		}
+
+		resolve(article);
+	});
+});
+
+// Gets Article Content for URL and sanitizes response HTML
+const getArticleContent = async (url) => {
+	const article = await getArticle(url);
+	const { content } = article;
+
+	if (content) {
+		return cleanHTML(content);
+	}
+
+	return null;
+};
+
+module.exports = { getArticleContent };
---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+const { fetchFeed } = require('./fetchFeed');
+const { getArticleContent } = require('./getArticleContent');
+
+module.exports = {
+	fetchFeed,
+	getArticleContent,
+};
