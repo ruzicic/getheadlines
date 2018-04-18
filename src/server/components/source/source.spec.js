d8166e32af49b0c3625e5b18b49350d4d55470f8
Mladen Ruzicic <ruzicic@gmail.com>
2018-04-19 00:28:15 +0200

    Fix source test

--- src/server/components/source/source.spec.js
+++ src/server/components/source/source.spec.js
@@ -2,6 +2,13 @@ import { expect } from 'chai';
 import request from 'supertest';
 import app from '../../../config/express';
 import * as SourceController from './sourceController';
+import * as UserController from '../user/userController';
+
+const USER_MOCK = {
+	name: 'TEST_USER',
+	email: 'test@user.com',
+	password: 'qwerty12345',
+};
 
 const MOCK = {
 	name: 'Not Blank',
@@ -16,7 +23,38 @@ const MOCK = {
 	period: 30,
 };
 
+let token;
+
+function loginUser() {
+	return (done) => {
+		request(app)
+			.post('/api/auth/login')
+			.send({
+				email: USER_MOCK.email,
+				password: USER_MOCK.password,
+			})
+			.expect(200)
+			.end((err, res) => {
+				// eslint-disable-next-line prefer-destructuring
+				token = res.body.message.token;
+				return done();
+			});
+	};
+}
+
 describe('Source', () => {
+	before((done) => {
+		UserController.deleteUserByEmail(USER_MOCK.email)
+			.then(() => UserController.addUser(USER_MOCK)
+				.then(() => done()));
+	});
+	before(loginUser());
+
+	after((done) => {
+		UserController.deleteUserByEmail(USER_MOCK.email)
+			.then(() => done());
+	});
+
 	describe('POST /api/sources', () => {
 		beforeEach((done) => {
 			SourceController.deleteSource(MOCK.url).then(() => done());
@@ -25,6 +63,7 @@ describe('Source', () => {
 		it('Should add new source', (done) => {
 			request(app)
 				.post('/api/sources')
+				.set('Authorization', `Bearer ${token}`)
 				.send(MOCK)
 				.expect(201)
 				.expect((res) => {
@@ -52,6 +91,7 @@ describe('Source', () => {
 		it('Should delete source', (done) => {
 			request(app)
 				.delete('/api/sources')
+				.set('Authorization', `Bearer ${token}`)
 				.query({ url: MOCK.url })
 				.expect(200)
 				.expect((res) => {
