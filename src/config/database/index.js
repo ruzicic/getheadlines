03647f8f06710ad311943d4ae91f8417dc387ea1
Mladen Ruzicic <ruzicic@gmail.com>
2018-04-21 19:34:41 +0200

    Add scripts for creating all needed tables

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-console */
+
 require('dotenv').config();
+
 const fs = require('fs');
 const util = require('util');
 const path = require('path');
@@ -14,11 +17,13 @@ const readFile = util.promisify(fs.readFile);
 		// Drop tables
 		const dropScript = await readFile(path.join(__dirname, 'drop_tables.sql'));
 		await client.query(dropScript.toString());
+
 		console.log('Tables dropped.');
 
 		// Create tables
 		const createScript = await readFile(path.join(__dirname, 'create_tables.sql'));
 		await client.query(createScript.toString());
+
 		console.log('Tables created.');
 	} catch (err) {
 		console.error(err);
